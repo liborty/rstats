eb9a79cc4b916ce0627b075813da8d6732770b41
L <457124+liborty@users.noreply.github.com>
2020-09-18 13:38:52 +1000

    added mutvecimpls.rs

---
+++ src/mutvecimpls.rs
@@ -0,0 +1,26 @@
+use crate::{MutVectors};
+use std::cmp::Ordering::Equal;
+
+impl MutVectors for &mut [f64] {
+    /// Scalar multiplication of a vector, mutates self
+    fn mutsmult(self, s: f64) {
+        self.iter_mut().for_each(|x| *x *= s);
+    }
+    /// Vector subtraction, mutates self
+    fn mutvsub(self, v: &[f64]) {
+        self.iter_mut().enumerate().for_each(|(i, x)| *x -= v[i])
+    }
+    /// Vector addition, mutates self
+    fn mutvadd(self, v: &[f64]) {
+        self.iter_mut().enumerate().for_each(|(i, x)| *x += v[i])
+    }
+    /// Mutate to unit vector
+    fn mutvunit(self) {
+        self.mutsmult(1_f64 / self.iter().map(|x| x.powi(2)).sum::<f64>().sqrt())
+    } 
+    /// Sorts a mutable `Vec<f64>` in place.  
+    /// It is the responsibility of the user to ensure that there are no NaNs etc.
+    fn mutsortf(self) {
+        self.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal))
+    }   
+}
