0410e4cb180da167033f26d2569beab5d9a254e1
L <457124+liborty@users.noreply.github.com>
2021-05-11 19:14:43 +1000

    Fixed benches/mybenchmark.rs for backwards compability's sake

--- benches/mybenchmark.rs
+++ benches/mybenchmark.rs
@@ -1,60 +1,5 @@
-use rstats::{VecVec};
+use rstats::{VecVec,Vecf64};
 use rstats::functions::{GI,genvec};
-
-/*
-use criterion::{black_box, criterion_group, criterion_main, Criterion}; //, BenchmarkId}
-fn compare_medians(c: &mut Criterion) {
-   let mut group = c.benchmark_group("Medians");
-   let d = 20;
-   let pts = genvec(d,60,5,7);
-   for i in [0usize,1usize,2usize].iter() {
-      group.bench_with_input(BenchmarkId::new("gmedian", i), i, 
-           |b, i| b.iter(|| {
-              let p = pts.get(i*d .. (i+1)*d).unwrap();
-              p.gmedian(black_box(d),black_box(1_e-7)) })); };
-   for i in [0usize,1usize,2usize].iter() {          
-      group.bench_with_input(BenchmarkId::new("nmedian", i), i, 
-           |b, i| b.iter(|| {
-              let p = pts.get(i*d .. (i+1)*d).unwrap();
-              p.gmedian(black_box(d),black_box(1_e-7)) }));
-   };
-   group.finish();
-}
-criterion_group!(benches, compare_medians);
-criterion_main!(benches);
-
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId}
-pub fn criterion_benchmark(c: &mut Criterion) {
-   let pts = genvec(2,2000,11,13);
-   c.bench_function("GMedian", |b| b.iter(||
-      pts.as_slice().gmedian(black_box(2),black_box(1_e-1))));
-   c.bench_function("NMedian", |b| b.iter(||
-      pts.as_slice().nmedian(black_box(2),black_box(1_e-1))));
-}
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
-*/
-/*
-use devtimer::run_benchmark;
-pub fn main() {
-   const D:usize = 50;
-   const N:usize = 100; 
-
-   let res_gmedian = run_benchmark(10, |i| {
-      let pts = genvec(D,N,(i+1) as u32,N as u32);
-      let (_dg,_) = pts.as_slice().gmedian(D, 1./((i+1)as f64).powi(i as i32)).unwrap();
-   });
-   println!("gmedian results");
-   res_gmedian.print_stats();
-
-   let res_nmedian = run_benchmark(10, |i| {
-      let pts = genvec(D,N,(i+1) as u32,N as u32);
-      let (_dg,_) = pts.as_slice().nmedian(D, 1./((i+1)as f64).powi(i as i32)).unwrap();
-   });
-   println!("nmedian results"); 
-   res_nmedian.print_stats()
-}
-*/
 use devtimer::DevTime;
 
 fn main() {
@@ -77,7 +22,7 @@ fn main() {
       let g = pts.as_slice().nmedian(EPS);
       cmplx.stop_timer("nmedian").unwrap();
       sumtime += cmplx.time_in_nanos("nmedian").unwrap();
-      sumg += pts.as_slice().ecc(&g);  
+      sumg += pts.eccnonmember(&g).vmag();  
    };
    println!("\nSum of nmedian residual errors: {} time: {} ", GI(sumg), GI(sumtime));
   
