89978daec0ce2dd0c154ce6b57dd6cfc8c3f568d
L <457124+liborty@users.noreply.github.com>
2022-08-30 23:07:13 +1000

    householder

--- src/vecvec.rs
+++ src/vecvec.rs
@@ -27,25 +27,6 @@ impl<T> VecVec<T> for &[Vec<T>]
         data.transpose().iter().map(|v| v.vunit()).collect::<Vec<Vec<f64>>>().transpose()
     }
 
-/* todo
-function [R,U] = house_qr(A)
-    % Householder reflections for QR decomposition.
-    % [R,U] = house_qr(A) returns
-    % R, the upper triangular factor, and
-    % U, the reflector generators for use by house_apply.    
-    H = @(u,x) x - u*(u'*x);
-    [m,n] = size(A);
-    U = zeros(m,n);
-    R = A;
-    for j = 1:min(m,n)
-        u = house_gen(R(j:m,j));
-        U(j:m,j) = u;
-        R(j:m,j:n) = H(u,R(j:m,j:n));
-        R(j+1:m,j) = 0;
-    end
-end
-*/
-
     /// Householder's method returning matrices (U,R), where 
     /// U are the reflector generators for use by house_apply.
     /// R is the upper triangular decomposition factor. 
